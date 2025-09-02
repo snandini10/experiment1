3a58b6e36d341eefb77954b106db2a8de207f01a
snandini10 <snandini_cse230551@mgit.ac.in>
2025-09-02 21:53:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.getElementById("ecetForm").addEventListener("submit", function (e) {
   e.preventDefault(); // Prevent form from submitting
 
   const phone = document.getElementById("phone").value;
-
-  if (!/^\d{10}$/.test(phone)) {
+  
+  if (!/^(\+91)?\d{10}$/.test(phone)) {
     alert("Phone number must be 10 digits.");
     return;
   }
